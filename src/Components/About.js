d4ca1c52ff22dfa7a0541c6ac3c6c904347e7f7c
Manvir <manvirsinghd@hmail.com>
2023-07-27 13:20:44 +0530

    first commit

---
+++ src/Components/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+//import AboutBackground from "../Assets/about-background.png";
+import AboutBackgroundImage from "../Assets/about-background-image.png";
+import { BsFillPlayCircleFill } from "react-icons/bs";
+
+const About = () => {
+  return (
+    <div className="about-section-container">
+      <div className="about-section-image-container">
+        <img src={AboutBackgroundImage} alt="" />
+      </div>
+      <div className="about-section-text-container">
+        <p className="primary-subheading">About</p>
+        <h1 className="primary-heading">
+        Our love, for your love
+        </h1>
+        <p className="primary-text">
+        Our bakery is set up to cater specifically to the cake lover, our bakery is open to everyone but only the most special order. It would be great if our bakery can make you get your own cake because that's where our family comes from!
+        </p>
+        <div className="about-buttons-container">
+          <button className="secondary-button">Learn More</button>
+          <button className="watch-video-button">
+            <BsFillPlayCircleFill /> Watch Video
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
